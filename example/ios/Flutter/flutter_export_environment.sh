780b631567193cf122ee774c1ef855f74fec338f
aboutmydreams <aboutmydreams@163.com>
2019-12-25 15:36:33 +0800

    copy to github

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/dengwenhao/Library/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/dengwenhao/coding/learn/micro_sparrow/flutter_webview_plugin-0.3.4/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/dengwenhao/Library/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
